e3be8cd16c0c78daf59bf9c42abe0fceef7ebee3
Anton Hrytsevich <antongritsevich@coherentsolutions.com>
2022-01-11 19:22:07 +0300

    Initial work commit

---
+++ TestMultithread.cpp
@@ -0,0 +1,37 @@
+﻿// TestMultithread.cpp : Defines the entry point for the application.
+//
+
+#include "TestMultithread.h"
+#include <condition_variable>
+
+
+int main() {
+    using namespace std;
+    using namespace std::chrono_literals;
+    condition_variable variable;
+    SpeedMessuare metr;
+    uint64_t test_number = 0x3235434f34534FD0;
+    ThreadPool pool{ test_number };
+
+    for (auto i = 2; i < test_number; ) {
+        uint64_t left_bound = i;
+        uint64_t right_bound = (i + 0xFF) > test_number ? (test_number - 1) : (i + 0xFF); 
+        pool.add_thread(i, i + 0xFF);
+
+        uint32_t m1 = metr.meassure();
+        cout << m1 << endl;
+
+        //if (m1 > 200 ) std::this_thread::sleep_for(2000ms);
+        if (pool.result() == true) break;
+    }
+    pool.wait();
+
+    if (pool.result()) {
+        cout << "Number: "<< test_number << " is NOT prime." << endl;
+    }
+    else {
+        cout << "Number: " << test_number << " is prime." << endl;
+    }
+
+    return 0;
+}
---
+++ TestMultithread.h
@@ -0,0 +1,93 @@
+﻿// TestMultithread.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <stdint.h>
+#include <iostream>
+#include <vector>
+#include <future>
+#include <chrono>
+#include <atomic>
+
+class ThreadPool final {
+
+private:
+    std::vector<std::thread> _threads;
+
+    std::atomic<bool>        _stopping = false;
+    std::atomic<bool>        _is_not_prime = false;
+    uint64_t                 _test_number = 0;
+
+    bool prime_test(uint64_t start, uint64_t end) {
+        while (start++ < end && !_stopping) {
+            if ((_test_number % start) == 0) { // is prime?
+                _is_not_prime = true;
+                _stopping = true;
+                return true;
+            }
+        }
+        return false;
+    }
+
+public:
+    explicit ThreadPool(uint64_t number) :
+        _test_number(number) {
+    }
+
+    ~ThreadPool() {
+        wait();
+    }
+
+    ThreadPool(const ThreadPool& c) = delete;
+    ThreadPool& operator=(const ThreadPool& c) = delete;
+
+    void add_thread(uint64_t left_bound, uint64_t right_bound) {
+        if (_stopping) return;
+
+        _threads.emplace_back([=] {prime_test(left_bound, right_bound); });
+    }
+
+    void wait() noexcept {
+        for (auto& thread : _threads)
+            if (thread.joinable()) thread.join();
+    }
+
+    bool result() noexcept  { return _is_not_prime; }
+
+};
+
+class SpeedMessuare {
+public:
+    SpeedMessuare() {
+        auto start_point = std::chrono::high_resolution_clock::now();
+        auto a = meassure_unit();
+        auto end_point = std::chrono::high_resolution_clock::now();
+        auto duration = end_point - start_point;
+        _standart_duration = static_cast<double>(duration.count());
+    }
+
+    ~SpeedMessuare() {}
+    SpeedMessuare(const SpeedMessuare& c) = delete;
+    SpeedMessuare& operator=(const SpeedMessuare& c) = delete;
+
+    uint32_t meassure() {
+        auto start_point = std::chrono::high_resolution_clock::now();
+        auto a = meassure_unit();
+        auto end_point = std::chrono::high_resolution_clock::now();
+        double duration = (end_point - start_point).count();
+        
+        auto delta = (duration - _standart_duration);
+        auto midl = (delta / _standart_duration) * 100;
+        return static_cast<uint32_t>(midl);
+    }
+
+private:
+    uint32_t meassure_unit() {
+        volatile uint32_t i = 0;
+        while (i < 0xFFFFFFF) i++;
+        return i;
+    }
+
+    double _standart_duration;
+};
